77448d5788349a66935ee0467eaee79cebd33f01
Reda Mohsen <111102980+reda-mohsen@users.noreply.github.com>
2023-06-11 21:18:33 +0300

    Add indoor.py

---
+++ indoor.py
@@ -0,0 +1,7 @@
+"""
+prompts the user for input and then outputs that same input in lowercase.
+Punctuation and whitespace should be outputted unchanged.
+"""
+lower_str = input("What you say? ").lower()
+
+print(f"{lower_str}")
