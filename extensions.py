554b2b4df95416c1ff3b60ccb4ba019c3983e70a
Reda Mohsen <111102980+reda-mohsen@users.noreply.github.com>
2023-08-22 05:11:53 +0300

    Add extensions.py

---
+++ extensions.py
@@ -0,0 +1,32 @@
+"""
+ implement a program that prompts the user for the name of a file
+ and then outputs that file’s media type
+ if the file’s name ends, case-insensitively, in any of these suffixes:
+
+.gif
+.jpg
+.jpeg
+.png
+.pdf
+.txt
+.zip
+If the file’s name ends with some other suffix or has no suffix at all,
+output application/octet-stream instead, which is a common default.
+"""
+
+filename = input("File name: ").strip().lower()
+
+if filename.endswith(".gif"):
+    print("image/gif")
+elif filename.endswith(".jpg") or filename.endswith(".jpeg"):
+    print("image/jpeg")
+elif filename.endswith(".png"):
+    print("image/png")
+elif filename.endswith(".pdf"):
+    print("application/pdf")
+elif filename.endswith(".txt"):
+    print("text/plain")
+elif filename.endswith(".zip"):
+    print("application/zip")
+else:
+    print("application/octet-stream")
