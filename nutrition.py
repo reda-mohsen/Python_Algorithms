9ef416ef86939bd8bab4572801cf3e462ebe0bb7
Reda Mohsen <111102980+reda-mohsen@users.noreply.github.com>
2023-08-23 23:51:28 +0300

    Add nutrition.py

---
+++ nutrition.py
@@ -0,0 +1,37 @@
+"""
+a program that prompts consumers users to input a fruit (case-insensitively) and
+then outputs the number of calories in one portion of that fruit, per the FDA’s poster for fruits,
+which is also available as text. Capitalization aside, assume that users will input fruits exactly as written in the poster
+(e.g., strawberries, not strawberry). Ignore any input that isn’t a fruit.
+"""
+
+def main():
+    Fruits = {
+        "Apple": "130",
+        "Avocado": "50",
+        "Banana": "110",
+        "Cantaloupe": "50",
+        "Grapefruit": "60",
+        "Grapes": "90",
+        "Honeydew Melon": "50",
+        "Kiwifruit": "90",
+        "Lemon": "15",
+        "Lime": "20",
+        "Nectarine": "60",
+        "Orange": "80",
+        "Peach": "60",
+        "Pear": "100",
+        "Pineapple": "50",
+        "Plums": "70",
+        "Strawberries": "100",
+        "Sweet Cherries": "100",
+        "Tangerine": "50",
+        "Watermelon": "80"
+    }
+
+    fruit_item = input("Item: ").strip().title()
+    for fruit in Fruits:
+        if fruit_item == fruit:
+            print("Calories:",Fruits[fruit])
+
+main()
