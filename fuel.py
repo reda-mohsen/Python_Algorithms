239291dbc28e2094007003dfe64971851535b2f4
Reda Mohsen <111102980+reda-mohsen@users.noreply.github.com>
2023-10-02 09:20:04 +0300

    Update fuel.py

--- fuel.py
+++ fuel.py
@@ -5,17 +5,21 @@ as a percentage rounded to the nearest integer, how much fuel is in the tank.
 If, though, 1% or less remains, output E instead to indicate that the tank
 is essentially empty. And if 99% or more remains,
 output F instead to indicate that the tank is essentially full.
+
+convert expects a str in X/Y format as input, wherein each of X and Y is an integer,
+and returns that fraction as a percentage rounded to the nearest int between 0 and 100, inclusive.
+If X and/or Y is not an integer, or if X is greater than Y, then convert should raise a ValueError.
+If Y is 0, then convert should raise a ZeroDivisionError.
+gauge expects an int and returns a str that is:
+"E" if that int is less than or equal to 1,
+"F" if that int is greater than or equal to 99,
+and "Z%" otherwise, wherein Z is that same int.
 """
+import sys
 
 def main():
     fraction = get_fraction("Fraction: ")
-    percentage = round(fraction*100)
-    if percentage < 1:
-        print ("E")
-    elif percentage>99:
-        print("F")
-    else:
-        print(f"{percentage}%")
+    print(gauge(convert(fraction)))
 
 def get_fraction(prompt):
     while True:
@@ -26,11 +30,35 @@ def get_fraction(prompt):
                 y = int(fraction[1])
                 if x>y:
                     continue
+                elif y==0:
+                    continue
                 else:
-                    return x/y
+                    return f"{fraction[0]}/{fraction[1]}"
             else:
                 continue
-        except (ValueError, ZeroDivisionError):
+        except ValueError:
             pass
-        
-main()
+
+def convert(fraction):
+    try:
+        fraction = fraction.split("/")
+        x = int(fraction[0])
+        y = int(fraction[1])
+        value = (x/y)*100
+    except ValueError:
+        raise
+    except ZeroDivisionError:
+        raise
+    else:
+        return int(round(value))
+
+def gauge(percentage):
+    if percentage <= 1:
+        return "E"
+    elif percentage>=99:
+        return "F"
+    else:
+        return f"{percentage}%"
+
+if __name__ == "__main__":
+    main()
